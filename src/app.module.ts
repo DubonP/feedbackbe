34bcd26703e3c5db3d017105e42cf352ca0b8fad
PauloRossi <paulogustavorossi@gmail.com>
2023-02-16 18:09:59 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('HOST') || 'localhost',
+        port: +configService.get('PORT') || 3306,
+        username: configService.get('USERNAME') || 'admin',
+        password: configService.get('PASSWORD') || 'root',
+        database: configService.get('DATABASE') || 'admin',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
